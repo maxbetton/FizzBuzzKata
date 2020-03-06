ceeaedade6219dd4301644f20b07bf44353bc5cd
Maxime Betton <maxime.betton@ynov.com>
2020-03-06 12:49:46 +0100

    refacto sum two numbers

--- src/com/ynov/stringCalculator/main/StringCalculator.java
+++ src/com/ynov/stringCalculator/main/StringCalculator.java
@@ -3,8 +3,10 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty())
             return 0;
-        else if (numbers.contains(","))
-            return 3;
+        else if (numbers.contains(",")) {
+            String[] split = numbers.split(",");
+            return Integer.parseInt(split[0]) + Integer.parseInt(split[1]);
+        }
         else
             return Integer.parseInt(numbers);
     }
