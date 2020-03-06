625b4dc9824a5dfe549e5cdda976ce633bc60eb5
Maxime Betton <maxime.betton@ynov.com>
2020-03-06 15:33:41 +0100

    green custom separator

--- src/com/ynov/stringCalculator/main/StringCalculator.java
+++ src/com/ynov/stringCalculator/main/StringCalculator.java
@@ -1,23 +1,35 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     public int add(String numbers) {
         int result = 0;
         if (numbers.isEmpty())
             return 0;
-        else if (numbers.contains(",")) {
-            String[] split = numbers.split("[,\n]");
+        else {
+            String[] split = tokenize(numbers);
             for (String string: split
             ) {
                 result += convert(string);
             }
             return result;
         }
-        else
-            return convert(numbers);
     }
 
     public Integer convert(String s) {
         return Integer.parseInt(s);
     }
 
+    private static String[] tokenize(String numbers) {
+        if (numbers.startsWith("//")) {
+            Matcher m = Pattern.compile("//(.)\n(.*)").matcher(numbers);
+            m.matches();
+            String customDelimiter = m.group(1);
+            String resultNumber = m .group(2);
+            return resultNumber.split(customDelimiter);
+        }
+        String[] tokens = numbers.split(",|\n");
+        return tokens;
+    }
 }
