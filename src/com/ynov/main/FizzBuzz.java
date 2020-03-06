614f88b785f21ade37e03508c7fd250f8b514aa3
Maxime Betton <maxime.betton@ynov.com>
2020-03-06 11:30:16 +0100

    refacto buzz

--- src/com/ynov/main/FizzBuzz.java
+++ src/com/ynov/main/FizzBuzz.java
@@ -1,13 +1,13 @@
 public class FizzBuzz {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 
     public String check(int number) {
         if(number%3 == 0)
             return "Fizz";
-        else if (number == 5)
+        else if (number%5 == 0)
             return "Buzz";
         return String.valueOf(number);
     }
