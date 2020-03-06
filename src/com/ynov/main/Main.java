bea320bdac72144fe85f09c8c52e112af63f1a4d
Maxime Betton <maxime.betton@ynov.com>
2020-03-06 11:01:57 +0100

    init

---
+++ src/com/ynov/main/Main.java
@@ -0,0 +1,8 @@
+package com.ynov.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/ynov/test/MainTest.java
@@ -0,0 +1,10 @@
+package com.ynov.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
