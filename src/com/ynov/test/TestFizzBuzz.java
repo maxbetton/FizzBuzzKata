aa361c757ccac8cac61bbc22c9a242c42a496472
Maxime Betton <maxime.betton@ynov.com>
2020-03-06 11:15:51 +0100

    clean

---
+++ src/com/ynov/test/TestFizzBuzz.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import com.ynov.main.Main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+@Test
+    public void shouldPrintFizzIfDividedByThree() {
+    Main main = new Main();
+    assertEquals()
+
+}
+}
