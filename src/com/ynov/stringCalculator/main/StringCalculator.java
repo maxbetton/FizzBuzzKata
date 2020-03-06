d691450da1a0443b0b1ff403364ba76b8b33f89b
Maxime Betton <maxime.betton@ynov.com>
2020-03-06 12:44:36 +0100

    green number one

--- src/com/ynov/stringCalculator/main/StringCalculator.java
+++ src/com/ynov/stringCalculator/main/StringCalculator.java
@@ -1,6 +1,9 @@
 public class StringCalculator {
 
     public int add(String number) {
-        return 0;
+        if (number.isEmpty())
+            return 0;
+        else
+            return 1;
     }
 }
