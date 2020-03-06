d6645d1d8ec6667b472ae27fb1c1adbfbf511312
Maxime Betton <maxime.betton@ynov.com>
2020-03-06 14:34:13 +0100

    green sum mumtiple numbers

--- src/com/ynov/stringCalculator/main/StringCalculator.java
+++ src/com/ynov/stringCalculator/main/StringCalculator.java
@@ -1,11 +1,16 @@
 public class StringCalculator {
 
     public int add(String numbers) {
+        int result = 0;
         if (numbers.isEmpty())
             return 0;
         else if (numbers.contains(",")) {
             String[] split = numbers.split(",");
-            return Integer.parseInt(split[0]) + Integer.parseInt(split[1]);
+            for (String string: split
+            ) {
+                result += Integer.parseInt(string);
+            }
+            return result;
         }
         else
             return Integer.parseInt(numbers);
